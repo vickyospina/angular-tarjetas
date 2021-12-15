f835e74cf5fce33a996dfae1c4e07f3f5c8b253b
Vicky Ospina <vicky.ospinagmail.com>
2021-12-15 13:50:56 -0500

    app tarjetas

---
+++ BackEnd/BackEnd/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BackEnd.Models;
+
+namespace BackEnd
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        //DBSet-Modelo-BaseDatos-getset
+    }
+}
